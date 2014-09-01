75e4ba4eb08c0e7cbd9940fc8ffcb27ea3a37b65
agus2409 <agustin.arevalo5@gmail.com>
2014-09-01 17:34:29 -0300

    Segundo commit
    Se pasaron los test de la función search.

--- code-katas-2/src/code_katas_2/core.clj
+++ code-katas-2/src/code_katas_2/core.clj
@@ -4,16 +4,26 @@
 (defn unpartial
   "Escribir una funcion que acepte una funcion parcial con cantidad de argumentos desconocida,
    retornar una funcion equivalente de n argumentos"
-  [f]
-  )
+  [f] 
+)
 
 
 (defn search
   "Dado un numero cualquiera de secuencias, cada una ya ordenada de menor a mayor, encontrar el numero
    mas chico que aparezca en todas las secuencias, las secuencias pueden ser infinitas."
   [& seqs]
+ ;dentro de una lazy-seq tomo el primero de una de las secuencias si esta en las restantes listas lo dejo sino lo tiro
+ ;mando recursivo con las nuevas listas
+ (defn remover [sequ]
+   (map #(if (= (first %) (first (sort (map first seqs)))) (drop 1 %) %) sequ))
+ 
+ 
+ (if (not-any? false? (map #(=(first %) (first (sort (map first seqs)))) seqs))
+   (first (sort (map first seqs)))
+   (recur (remover seqs))
   )
-
+ )
+   
 
 (defn intercalar
   "Escriba una funcion que tome un predicado de 2 argumentos, un valor y una coleccion, y
@@ -30,4 +40,8 @@
    La funcion debe aceptar una secuencia inicial de numeros, y devolver una secuencia infinita de compresiones, donde
    cada nuevo elemento es el elemento anterior comprimido."
   [secuencia]
-  )
+)
+
+
+
+(search [1 2 3 4 5 6 7 8 9] [0.5 3/2 7 19])
