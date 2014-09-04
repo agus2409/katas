76bb58e6b9502b03e48d28e20c9189fb18ff0252
agus2409 <agustin.arevalo5@gmail.com>
2014-09-04 11:16:42 -0300

    Cuarto Commit
    unpartial feo pero andando.

--- code-katas-2/src/code_katas_2/core.clj
+++ code-katas-2/src/code_katas_2/core.clj
@@ -5,6 +5,19 @@
   "Escribir una funcion que acepte una funcion parcial con cantidad de argumentos desconocida,
    retornar una funcion equivalente de n argumentos"
   [f]
+  
+ (fn[ & args]
+   (if (= (count args) 1)
+    (if (fn?(f(first (first args))))
+      ((unpartial (f(first (first args)))) (rest (first args)))
+      (f(first (first args)))
+   )
+    (if (fn?(f(first args)))
+      ((unpartial (f(first args))) (rest args))
+   )
+    )
+ )
+ )
 ;  def b(toto 10)
 ;  fb 15
 ;  fn? preguntar si es una funcion, si es devolver recursivo, sino devolver resultado
@@ -12,23 +25,22 @@
 ;   (let[res(f (first args))])
 ;     (if (fn? res)
 ;        (recur res(rest args))
-)
 
 
 (defn search
   "Dado un numero cualquiera de secuencias, cada una ya ordenada de menor a mayor, encontrar el numero
    mas chico que aparezca en todas las secuencias, las secuencias pueden ser infinitas."
   [& seqs]
- ;dentro de una lazy-seq tomo el primero de una de las secuencias si esta en las restantes listas lo dejo sino lo tiro
- ;mando recursivo con las nuevas listas
- (defn remover [sequ]
-   (map #(if (= (first %) (first (sort (map first sequ)))) (drop 1 %) %) sequ))
+;dentro de una lazy-seq tomo el primero de una de las secuencias si esta en las restantes listas lo dejo sino lo tiro
+;mando recursivo con las nuevas listas
+(defn remover [sequ x]
+  (map #(if (= (first %) x) (drop 1 %) %) sequ))
  
  
- (if (not-any? false? (map #(=(first %) (first (sort (map first seqs)))) seqs))
-   (first (sort (map first seqs)))
-   (recur (remover seqs))
- )
+(if (not-any? false? (map #(=(first %) (first (sort (map first seqs)))) seqs))
+  (first (sort (map first seqs)))
+  (recur (remover seqs (first (sort (map first seqs)))))
+)
 )
    
 
@@ -50,3 +62,11 @@
   [secuencia]
   ;(iterate funcion secuencia)
 )
+
+
+
+((unpartial (fn [a]
+              (fn [b]              
+                (fn [c]
+                  (fn [d]
+                    (+ a b c d)))))) 10 5 3 2)
